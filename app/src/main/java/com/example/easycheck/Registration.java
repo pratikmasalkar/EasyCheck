c8bd9b3ded8725b5cea5ec5dc038744850d70f96
pratikmasalkar <pratikmasalkar9730@gmail.com>
2024-01-25 22:58:42 +0530

    Solve the direct login problem

--- app/src/main/java/com/example/easycheck/Registration.java
+++ app/src/main/java/com/example/easycheck/Registration.java
@@ -143,7 +143,7 @@ public class Registration extends AppCompatActivity {
                             etRollno.setText("");
                             etPassword.setText("");
                             // Navigate to another activity
-                            startActivity(new Intent(Registration.this, LoginActivity.class));
+//                            startActivity(new Intent(Registration.this, LoginActivity.class));
                             overridePendingTransition(R.anim.slide_in_left, android.R.anim.slide_out_right);
                             finish();
 
